abb29541c322d0ecbc49112bebee564e6c193a6d
Joe DeSoto <modulusman@gmail.com>
2022-11-21 15:49:37 -0800

    Adds an About page and a button to naviagte to that page.
    Places MaterialApp within the runApp function so that the
    Navigation can be reached.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
-void main(List<String> args) {
-  runApp(const MyApp());
+void main() {
+  runApp(const MaterialApp(
+    title: 'How to: Nav',
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int count = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -21,44 +16,33 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blueGrey,
-          title: const Text('Hello from Flutter'),
+          title: const Text('Flutter Home!'),
         ),
-        body: ListView.builder(
-          itemBuilder: (_, index) {
-            return Container(
-              color: Colors
-                  .primaries[math.Random().nextInt(Colors.primaries.length)],
-              width: 500,
-              height: 500,
-              child: Text(
-                'Pressed `plus` $count times',
-                style: const TextStyle(fontSize: 32),
+        body: ElevatedButton(
+          child: const Text('About'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => const AboutScreen(),
               ),
             );
           },
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              count++;
-            });
-          },
-          child: const Icon(Icons.add),
-        ),
-        bottomNavigationBar: BottomNavigationBar(items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ]),
+      ),
+    );
+  }
+}
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: const Text('About Page'),
       ),
     );
   }
